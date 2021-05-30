a1d3fbaf31e82e7d5545c3d8ff701d664957b310
Thisal Tennakoon <62555789+thisal17@users.noreply.github.com>
2021-05-30 12:42:56 +0530

    Update settings.py

--- CrowdsourcedDataAnnotationPlatform/settings.py
+++ CrowdsourcedDataAnnotationPlatform/settings.py
@@ -133,7 +133,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'crowdsourceddataannotationplatform',
-        'USER': 'sep',
+        'USER': 'root',
         'PASSWORD': 'sep.2020',
         'HOST':'localhost',
         'PORT':'3308'
